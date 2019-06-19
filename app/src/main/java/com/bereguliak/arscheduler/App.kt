8ffd79009926697b19e90b443c727ea1143e4ed7
Yuriy Bereguliak <yuriy.bereguliak@inoxoft.com>
2019-06-19 18:37:43 +0300

    Removed temporary Fabric.

--- app/src/main/java/com/bereguliak/arscheduler/App.kt
+++ app/src/main/java/com/bereguliak/arscheduler/App.kt
@@ -1,20 +1,12 @@
 package com.bereguliak.arscheduler
 
 import android.app.Application
-import com.crashlytics.android.Crashlytics
-import io.fabric.sdk.android.Fabric
 
-class App: Application() {
+class App : Application() {
+
     //region Application
     override fun onCreate() {
         super.onCreate()
-        initCrashlytics()
-    }
-    //endregion
-
-    //region Utility API
-    private fun initCrashlytics() {
-        Fabric.with(this, Crashlytics())
     }
     //endregion
 }
