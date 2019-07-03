edf44077f0725af535d904725d4febb5fb82fe8d
Bereguliak <yuriy.bereguliak@inoxoft.com>
2019-07-03 22:57:50 +0300

    #1 : fixed problem with migration.

--- app/src/main/java/com/bereguliak/arscheduler/core/ui/BaseActivity.kt
+++ app/src/main/java/com/bereguliak/arscheduler/core/ui/BaseActivity.kt
@@ -3,8 +3,8 @@ package com.bereguliak.arscheduler.core.ui
 import android.os.Bundle
 import android.support.annotation.IdRes
 import android.support.annotation.LayoutRes
-import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
+import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
 
 abstract class BaseActivity : AppCompatActivity() {
 
