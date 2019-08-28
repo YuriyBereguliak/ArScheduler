2cded0356911f5aff97a99e6d6cd27ba3d65ea98
Yuriy Bereguliak <yuriy.bereguliak@inoxoft.com>
2019-08-28 17:49:01 +0300

    #13 : added description support.

--- app/src/main/java/com/bereguliak/arscheduler/ui/fragments/details/adapter/CalendarDetailsViewHolder.kt
+++ app/src/main/java/com/bereguliak/arscheduler/ui/fragments/details/adapter/CalendarDetailsViewHolder.kt
@@ -2,6 +2,7 @@ package com.bereguliak.arscheduler.ui.fragments.details.adapter
 
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import com.bereguliak.arscheduler.R
 import com.bereguliak.arscheduler.model.CalendarEvent
@@ -14,13 +15,29 @@ class CalendarDetailsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemVi
     private val startTime = itemView.findViewById<TextView>(R.id.eventStartTime)
     private val endTime = itemView.findViewById<TextView>(R.id.eventEndTime)
 
+    private val description = itemView.findViewById<TextView>(R.id.descriptionTextView)
+    private val descriptionIcon = itemView.findViewById<ImageView>(R.id.descriptionIconImageView)
+
     //region CalendarDetailsViewHolder
     fun bind(event: CalendarEvent) {
         title.text = event.title
-
+        handleEventDescription(event)
         val simpleDateFormat = SimpleDateFormat("HH:mm", Locale.getDefault())
         startTime.text = simpleDateFormat.format(event.startTime)
         endTime.text = simpleDateFormat.format(event.endTime)
+    }
+    //endregion
+
+    //region Utility API
+    private fun handleEventDescription(event: CalendarEvent) {
+        if (event.description.isNullOrEmpty()) {
+            descriptionIcon.visibility = View.GONE
+            description.visibility = View.GONE
+        } else {
+            description.visibility = View.VISIBLE
+            descriptionIcon.visibility = View.VISIBLE
+            description.text = event.description
+        }
     }
     //endregion
 }
