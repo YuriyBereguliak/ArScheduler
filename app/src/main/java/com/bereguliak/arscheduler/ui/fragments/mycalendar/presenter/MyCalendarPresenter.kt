4fcfabc7790662b5c6277ad751fb276940c88759
Yuriy Bereguliak <yuriy.bereguliak@inoxoft.com>
2019-08-30 13:35:05 +0300

    #15 : created base structure.

---
+++ app/src/main/java/com/bereguliak/arscheduler/ui/fragments/mycalendar/presenter/MyCalendarPresenter.kt
@@ -0,0 +1,7 @@
+package com.bereguliak.arscheduler.ui.fragments.mycalendar.presenter
+
+import com.bereguliak.arscheduler.core.presenter.BaseCoroutinePresenter
+import com.bereguliak.arscheduler.ui.fragments.mycalendar.MyCalendarContract
+
+class MyCalendarPresenter : BaseCoroutinePresenter(), MyCalendarContract.Presenter {
+}
---
+++ app/src/main/java/com/bereguliak/arscheduler/ui/fragments/mycalendar/view/MyCalendarArFragment.kt
@@ -0,0 +1,7 @@
+package com.bereguliak.arscheduler.ui.fragments.mycalendar.view
+
+import com.bereguliak.arscheduler.ui.fragments.mycalendar.MyCalendarContract
+import com.google.ar.sceneform.ux.ArFragment
+
+class MyCalendarArFragment : ArFragment(), MyCalendarContract.View {
+}
